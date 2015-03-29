0c982bffa7c6d21055e36d56a0b8ddddae4a3cb4
Adaryian Alexander <Adaryian@eCurrency.io>
2015-03-29 11:08:12 -0400

    Revert "Update #3"
    This reverts commit 2c1c0854a4059486499ea3ca40bf7c2b8c92e341.

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -10,7 +10,6 @@
 
 using namespace json_spirit;
 using namespace std;
-
 Value GetNetworkHashPS(int lookup, int height) {
     CBlockIndex *pb = pindexBest;
 
@@ -398,7 +397,7 @@ Value submitblock(const Array& params, bool fHelp)
         throw runtime_error(
             "submitblock <hex data> [optional-params-obj]\n"
             "[optional-params-obj] parameter is currently ignored.\n"
-            "Attempts to submit new block to network.\n"
+            "Attempts to submit new block to network.");
 
     vector<unsigned char> blockData(ParseHex(params[0].get_str()));
     CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION);
