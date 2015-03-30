9a6e8f70062c87975416cc5f29eafeebb864419d
Adaryian Alexander <Adaryian@eCurrency.io>
2015-03-30 12:04:55 -0400

    Organization Domain Update
    from ecurrency.org to ecurrency.io

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -151,7 +151,7 @@ int main(int argc, char *argv[])
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
     QApplication::setOrganizationName("eCurrency");
-    QApplication::setOrganizationDomain("ecurrency.org");
+    QApplication::setOrganizationDomain("ecurrency.io");
     if(GetBoolArg("-testnet")) // Separate UI settings for testnet
         QApplication::setApplicationName("eCurrency-Qt-testnet");
     else
